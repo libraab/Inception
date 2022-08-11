edbf0f8515d8d36dcc42808a5528eb228ab28c71
Asma Bouhlel boughdira <abouhlel@c1r6p3.42nice.fr>
2022-08-11 15:56:10 +0200

    script done

--- srcs/requirements/wordpress/testos.sh
+++ srcs/requirements/wordpress/testos.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 # si /var/www/html/ vide faire
-cp -rv /var/www/wordpress/* /var/www/html/
-cp -v /wp-config.php /var/www/html
-chown -Rv www-data:www-data /var/www/wordpress
-echo "Done."
-
-php-fpm7.3 -F
+DIR="/var/www/html"
+if ["$(ls -A $DIR)"];then
+    echo "NOT EMPTY."
+else
+    cp -rv /var/www/wordpress/* /var/www/html/
+    cp -v /wp-config.php /var/www/html
+    chown -Rv www-data:www-data /var/www/wordpress
+    echo "Done."
+    php-fpm7.3 -F
+fi
